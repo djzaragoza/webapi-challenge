de646e96f232242ceba9e3ee475bb01ccfa24594
DJ Zaragoza <zaragoza0126@gmail.com>
2019-08-03 11:37:44 -0700

    still working on projectrouter.js file

---
+++ routers/projectRouter.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const actionsdb = require("../data/helpers/actionModel");
+comst projectsdb = require("../data/helpers/projectModel");
+
+// it is CRUD time
+
+router.get("/", (req, res) => {
+    console.log("inside the get at root PROJECTS")
+    projectsdb
+    .get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        res.status(500).json({ err, message: "Unable to retrieve projects"});
+    });
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+    console.log("inside the get by ID PROJECTS")
+    const id = req.params.id;
+    console.log(id);
+    projectsdb
+    .get(id)
+    .then(project => {
+        if(project) {
+            res.status(200).json({
+                project
+            });
+        } else {
+            res.status(404).json({ message: "sorry no project"});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({err, message: "Unable to retrieve project"});
+    });
+});
+
+// CREATE
+
+router.post("/", (req, res) => {
+    projectsdb.insert(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(error => {
+            res.status(500).json({ error, message: "Error adding the project!"});
+        });
+})
+
+router.delete("/:id", validateProjectId, (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+        projectsdb
+        .remove(id)
+        then(response => {
+            res.status(200).json({
+                message: "project deleted"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "another error"
+            });
+        };
+            
+});
+
+router.put("/:id", validateProjectId, (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+        projectsdb
+        .update(id, req.body)
+        .then(response => {
+            res.status(200).json(
+                response
+            )
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "another error"
+            });
+        });
+});
+
